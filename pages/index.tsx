b64bfe8cfbea50214a32876c2f59aec59bc1e5f1
ninekiss <ninekiss2189@gmail.com>
2023-01-29 23:26:34 +0800

    chore: update content

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,11 +30,7 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello, I'm Nine Kiss. I'm a softeare enginer.]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Hello, I'm Nine Kiss. I'm a softeare enginer.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
